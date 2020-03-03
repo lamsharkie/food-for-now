df7a33cd6b63c22534287b324f97fbccb81f7c35
brody-rebne <bsrebne@gmail.com>
2020-03-03 11:54:51 -0800

    rendered results

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ function handleSearch(request, response) {
   let searchTerm = request.body.search;
   let url = `https://api.edamam.com/search?q=${searchTerm}&app_id=${process.env.EDAMAM_ID}&app_key=${process.env.EDAMAM_KEY}`;
   superagent.get(url).then(results => {
+    console.log(results.body.hits[0].recipe);
     const resultsArray = results.body.hits;
     const finalArray = resultsArray.map(recipe => {
       return new Recipe(recipe);
@@ -52,12 +53,12 @@ function handleSearch(request, response) {
 }
 
 function Recipe(obj){
-  this.label = obj.label;
-  this.image_url = obj.image;
-  this.ingredientLines = obj.ingredientLines;
-  this.recipe_url = obj.url;
-  this.dietLabels = obj.dietLabels;
-  this.healthLabels = obj.healthLabels;
+  this.label = obj.recipe.label;
+  this.image_url = obj.recipe.image;
+  this.ingredientLines = obj.recipe.ingredientLines;
+  this.recipe_url = obj.recipe.url;
+  this.dietLabels = obj.recipe.dietLabels;
+  this.healthLabels = obj.recipe.healthLabels;
 }
 
 // Turn on the DB and the Server
